124d31c14ea85dbab7bd3a665adafbb2d89931e1
Bruno Cardozo <bruno.cardozo.castro@gmail.com>
2023-07-31 21:12:46 -0300

    segunda entrega

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-let contador = 12;
+//PRIMERA ENTREGA  
+
+/*let contador = 12;
 let rta = "";
 let boletin = "Materias cursadas:\n";
 let promedio = 0;
@@ -20,4 +22,66 @@ if (promedio >=0.55){
     alert ("¡¡¡FELICITACIONES!!!\n Promovido al proximo año")
 }else{
     alert ("Tienes que recursar el año...")
-};
+};*/
+
+////////////////////////////////////////////////////////////////////////////////
+
+//SEGUNDA ENTREGA
+
+const materias = [];
+
+
+function agregarMateria() {
+    const materia = prompt("Ingrese el nombre de la materia");
+    const nota = parseFloat(prompt("Ingrese la nota"));
+
+    materias.push({
+    nombre: materia,
+    notas: [nota],
+    });
+}
+
+
+function calcularPromedio() {
+    let totalNotas = 0;
+    let totalExamenes = 0;
+
+    for (const materia of materias) {
+    for (const nota of materia.notas) {
+        totalNotas += nota;
+        totalExamenes++;
+    }
+    }
+
+    return totalNotas / totalExamenes;
+}
+
+function generarBoletin() {
+    let boletinDetallado = "Boletín Detallado:\n";
+
+    for (const materia of materias) {
+    boletinDetallado += `${materia.nombre}: ${materia.notas.join(", ")}\n`;
+    }
+
+    return boletinDetallado;
+}
+
+do {
+    agregarMateria();
+
+    const respuesta = prompt("¿Desea ingresar otra materia? (S/N)").toUpperCase();
+    if (respuesta !== "S") {
+    break;
+    }
+} while (true);
+
+const promedio = calcularPromedio();
+const boletin = generarBoletin();
+
+alert(boletin + "\nPromedio: " + promedio.toFixed(2));
+
+if (promedio >= 5.5) {
+    alert("¡¡¡FELICITACIONES!!!\nPromovido al próximo año");
+} else {
+    alert("Tienes que recursar el año...");
+}
